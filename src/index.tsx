c19b8adafcfe99a4e5ad29b79aa33b691b178b3f
David <50298235+HiThere157@users.noreply.github.com>
2022-11-17 14:17:19 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -30,6 +30,7 @@ root.render(
 );
 
 // TODO:
+// - Show current zoom level
 // - History Tab (command + args + result) 
 // - Settings (Set domain values / defaul to connected/trusted?)
 // - electron logger?
