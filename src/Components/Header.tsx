ef4e7a2f8ffc6eaec3ba3af896774d94e13a4f35
David <50298235+HiThere157@users.noreply.github.com>
2022-12-12 10:18:36 +0100

    link to homepage

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 import { electronAPI } from "../Helper/makeAPICall";
 
@@ -20,13 +21,13 @@ export default function Header() {
       style={{ gridArea: "header" }}
       className="winbar-drag-region select-none flex justify-between border-b-2 dark:bg-secondaryBg dark:border-primaryBorder"
     >
-      <div className="flex items-center">
+      <NavLink to="/" className="flex items-center">
         <img src="./icon.svg" alt="AD Tools Logo" className="mx-[1.1rem] h-6"></img>
         <div className="flex space-x-3 items-center whitespace-nowrap">
           <span className="font-bold text-xl">AD Tools</span>
           <span className="dark:text-foregroundAccent">{user}</span>
         </div>
-      </div>
+      </NavLink>
 
       <div className="winbar-no-drag flex space-x-2">
         <TableOfContents />
