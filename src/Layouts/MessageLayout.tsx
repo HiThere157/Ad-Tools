d48a1e67c666dbb8964cb8cef6c5dc865950ba22
David <50298235+HiThere157@users.noreply.github.com>
2023-02-24 20:47:42 +0100

    css

--- src/Layouts/MessageLayout.tsx
+++ src/Layouts/MessageLayout.tsx
@@ -3,7 +3,7 @@ type MessageLayoutProps = {
 };
 export default function MessageLayout({ children }: MessageLayoutProps) {
   return (
-    <div className="absolute z-[40] bottom-5 right-5 flex flex-col items-end gap-y-2">
+    <div className="absolute z-[40] bottom-5 right-5 flex flex-col gap-y-2">
       {children}
     </div>
   );
--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -45,7 +45,7 @@ export default function GroupPage() {
   };
 
   return (
-    <article className="flex flex-col gap-y-5 max-w-5xl">
+    <article className="space-y-5 max-w-5xl">
       <section>
         <h2 className="text-2xl font-bold mb-1" style={{ scrollMarginTop: "60px" }}>
           Domain Settings
