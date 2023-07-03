68aeca4bcd91aedb564ea747ea7b348dae846b26
David <50298235+HiThere157@users.noreply.github.com>
2023-07-03 17:26:30 +0200

    init

--- src/Components/InputBars/Hint.tsx
+++
@@ -1,7 +0,0 @@
-type HintProps = {
-  hint: string;
-  className?: string;
-};
-export default function Hint({ hint, className = "" }: HintProps) {
-  return hint ? <div className={"ml-1 text-whiteColorAccent " + className}>{hint}</div> : <></>;
-}
--- src/Layouts/TableLayout.tsx
+++
@@ -1,6 +0,0 @@
-type TableLayoutProps = {
-  children: React.ReactNode;
-};
-export default function TableLayout({ children }: TableLayoutProps) {
-  return <div className="flex flex-col gap-y-5">{children}</div>;
-}
