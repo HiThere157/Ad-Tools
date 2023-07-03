ed0e62887bca7e024e0760c1e53207e2150c43d0
David <50298235+HiThere157@users.noreply.github.com>
2023-07-03 17:56:35 +0200

    init tw

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+  </React.StrictMode>,
 );
