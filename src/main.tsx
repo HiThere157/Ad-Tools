aca524db3a11265690f6757eeb481d1da8216a24
David <50298235+HiThere157@users.noreply.github.com>
2023-07-04 22:48:41 +0200

    init router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>,
 );
