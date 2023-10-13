a5faa245c5976144bcbd9812918ed4ddb0af74e2
David <50298235+HiThere157@users.noreply.github.com>
2023-10-13 21:58:01 +0200

    persist

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,11 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 
 import preferencesReducer from "./preferences";
 
+const reducers = combineReducers({
+  preferences: preferencesReducer,
+});
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
 const store = configureStore({
-  reducer: {
-    preferences: preferencesReducer,
-  },
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export default store;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-import { navigationLinks } from "./Config/navigation";
 import store from "./Redux/store";
+import { navigationLinks } from "./Config/navigation";
 
 import RootLayout from "./Layout/Root";
 
@@ -14,15 +16,17 @@ import "./scrollbar.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
-        <RootLayout>
-          <Routes>
-            {navigationLinks.flat().map((link, linkIndex) => (
-              <Route key={linkIndex} path={link.href} element={link.page} />
-            ))}
-          </Routes>
-        </RootLayout>
-      </HashRouter>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <HashRouter>
+          <RootLayout>
+            <Routes>
+              {navigationLinks.flat().map((link, linkIndex) => (
+                <Route key={linkIndex} path={link.href} element={link.page} />
+              ))}
+            </Routes>
+          </RootLayout>
+        </HashRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
