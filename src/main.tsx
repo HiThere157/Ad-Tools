ca4a6127cb8360a1aa61ce96bdd5d4f0e7638a57
David <50298235+HiThere157@users.noreply.github.com>
2023-11-26 20:31:34 +0100

    cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ import { navigationLinks } from "./Config/navigation";
 import RootLayout from "./Layout/RootLayout";
 
 import "./index.css";
-import "./scrollbar.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
