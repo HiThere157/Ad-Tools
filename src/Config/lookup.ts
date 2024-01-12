f4b38c796f888ac438930c177d1edf272aff2321
David <50298235+HiThere157@users.noreply.github.com>
2024-01-12 13:38:56 +0100

    lookup

--- src/Config/lookup.ts
+++ src/Config/lookup.ts
@@ -3,16 +3,43 @@ export const friendlyNames: PartialRecord<string, string> = {
   key: "Key",
   value: "Value",
 
-  // Ad Objects
+  // Ad/Azure Objects
+  DisplayName: "Display Name",
   DistinguishedName: "Distinguished Name",
   UserPrincipalName: "User Principal Name",
   SamAccountName: "SAM Account Name",
   GroupCategory: "Group Category",
   ObjectClass: "Object Class",
+  ObjectId: "Object ID",
+  ObjectType: "Object Type",
+  AttributeName: "Attribute Name",
+  AttributeValue: "Attribute Value",
+  LastOriginatingChangeTime: "Timestamp",
+  DeviceOSType: "Device OS Type",
+  AccountEnabled: "Account Enabled",
+  IsManaged: "Is Managed",
+  ApproximateLastLogonTimestamp: "Last Logon",
   _Server: "Server",
 
   // Dns
   IPAddress: "IP Address",
+
+  // Printer
+  JobCount: "Job Count",
+  DriverName: "Driver Name",
+
+  // WMI
+  UserFriendlyName: "User Friendly Name",
+  SerialNumberID: "Serial Number ID",
+  YearOfManufacture: "Year of Manufacture",
+  InstallDate: "Install Date",
+  ReleaseDate: "Release Date",
+
+  // History
+  command: "Command",
+  timestamp: "Timestamp",
+  executionTime: "Execution Time",
+  success: "Success",
 };
 
 export function friendly(text: string) {
