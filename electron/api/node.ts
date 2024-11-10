0236c442a89dfa7b6824ab7ee1479d070b3c8cbd
David <50298235+HiThere157@users.noreply.github.com>
2024-11-10 01:30:27 +0100

    ts fix

--- electron/api/node.ts
+++ electron/api/node.ts
@@ -3,7 +3,7 @@ const packageJson = require("../../package.json");
 export function getElectronEnvironment(): ElectronEnvironment {
   return {
     executingUser: `${process.env.USERDOMAIN}\\${process.env.USERNAME}`,
-    appVersion: "v" + packageJson.version ?? "",
+    appVersion: "v" + packageJson.version,
     appChannel: process.env.AD_TOOLS_PRERELEASE === "true" ? "beta" : "stable",
   };
 }
